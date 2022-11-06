84472891015564127af632557812912e63a806ef
cmd41par <cmd41par@hotmail.com>
2022-11-06 07:27:20 -0500

    updated helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,17 @@
-print("hello world")
+import pytest
 
+
+import pytest
+import pandas as pd
+import os
+import getpass
+import sys
+
+print("hello world 1")
+print("added pandas")
+
+d = pd.array("")
+
+print("Current Directory: " + os.getcwd())
+print("Current UserID: " + getpass.getuser())
+print("Current Python: " + sys.version)
